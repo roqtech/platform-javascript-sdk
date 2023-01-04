fd3fe64841d81977fba7e0c48fa35ce414c037f7
Nikolay Savin <nikolay.savin@roq.tech>
2023-01-04 13:40:40 +0100

    Update default host to mars.pp

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const defaultHost = 'http://localhost:3002';
+export const defaultHost = 'https://mars-pp.roq-platform.com';
 export const defaultGraphqlEndpoint = '/v01/server/graphql';
 export const defaultCreateTokenEndpoint = '/v01/authorize/serviceAccount';
 export const defaultServiceAccountEmail = 'project-service-account@roq.tech';
