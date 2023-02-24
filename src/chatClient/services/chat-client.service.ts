4a691ce8348d0066fcdd6099f15b990c9fc91cda
Nikolay Savin <nikolay.savin@roq.tech>
2023-02-24 11:45:11 +0100

    Release new version of node.js SDK with tags assign

--- src/chatClient/services/chat-client.service.ts
+++ src/chatClient/services/chat-client.service.ts
@@ -4,10 +4,14 @@ export class ChatClientService {
   private gqlSdk: Sdk;
   public createConversation: Sdk['createConversation'];
   public deleteConversation: Sdk['deleteConversation'];
+  public assignTagsToConversation: Sdk['assignTagsToConversation'];
+  public unassignTagsFromConversation: Sdk['unassignTagsFromConversation'];
 
   constructor(gqlSdk: Sdk) {
     this.gqlSdk = gqlSdk;
     this.createConversation = this.gqlSdk.createConversation;
     this.deleteConversation = this.gqlSdk.deleteConversation;
+    this.assignTagsToConversation = this.gqlSdk.assignTagsToConversation;
+    this.unassignTagsFromConversation = this.gqlSdk.unassignTagsFromConversation;
   }
 }
